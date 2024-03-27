16ee6380bd5a2dcce1ea17063244dc14d389c51d
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-27 13:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let squares=document.querySelectorAll(".square")
+squares.forEach((square,index)=>{
+    square.addEventListener("mouseenter",(eventDetails)=>{
+        let boxId=eventDetails.target.id
+        squares.forEach(boxes=>{
+            if(boxes.id!=boxId){
+                boxes.classList.remove("square")
+                boxes.classList.add("coffee")
+            }
+        })
+       
+    })
+
+    square.addEventListener("mouseleave",(eventDetails)=>{
+        squares.forEach(boxes=>{
+            boxes.classList.remove("coffee")
+            boxes.classList.add("square")
+        })
+    })
+})
